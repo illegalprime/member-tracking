e0dd69a8f7f3cf037b0d7260d66fb6b51bacdd99
Michael Eden <themichaeleden@gmail.com>
2023-06-27 22:47:55 -0400

    ui: combine sources into main table

--- lib/member_tracking_web/live/members_live.ex
+++ lib/member_tracking_web/live/members_live.ex
@@ -2,10 +2,19 @@ defmodule MemberTrackingWeb.MembersLive do
   use MemberTrackingWeb, :live_view
   alias MemberTracking.Repo
   alias MemberTracking.Google.GroupMember
+  alias MemberTracking.Paypal.Subscriber
 
   def mount(_params, _session, socket) do
+    google = Repo.all(GroupMember)
+    |> Enum.map(fn g -> {g.email, %{google: g}} end)
+    |> Map.new()
+    paypal = Repo.all(Subscriber)
+    |> Enum.map(fn p -> {p.email, %{paypal: p}} end)
+    |> Map.new()
+    people = Map.merge(google, paypal, fn _k, g, p -> Map.merge(g, p) end)
+
     socket = socket
-    |> assign(google_group_members: Repo.all(GroupMember))
+    |> assign(people: people)
     {:ok, socket}
   end
 end
